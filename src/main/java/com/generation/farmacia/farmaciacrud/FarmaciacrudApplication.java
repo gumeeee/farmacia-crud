2dca5792aa66ebfa52498066adc07951145de658
gumeeee <gaxco123@gmail.com>
2024-03-26 09:51:31 -0300

    Iniciando projeto

---
+++ src/main/java/com/generation/farmacia/farmaciacrud/FarmaciacrudApplication.java
@@ -0,0 +1,13 @@
+package com.generation.farmacia.farmaciacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciacrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciacrudApplication.class, args);
+	}
+
+}
