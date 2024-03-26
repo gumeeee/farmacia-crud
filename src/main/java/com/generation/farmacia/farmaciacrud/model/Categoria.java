96b4a5472e6ff0ded1b7a29e36b6309520614b48
gumeeee <gaxco123@gmail.com>
2024-03-26 11:20:14 -0300

    Crud completo de categoria com 6 métodos

---
+++ src/main/java/com/generation/farmacia/farmaciacrud/model/Categoria.java
@@ -0,0 +1,38 @@
+package com.generation.farmacia.farmaciacrud.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo nome é obrigatório")
+	@Size(min = 5, max = 100, message = "O atributo nome deve conter no mínimo 5 Caracteres, e no máximo 100")
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
